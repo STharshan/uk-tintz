1556d07a1140577e21def1f217ae5a9de8c8051c
shandiru <msddiru@gmail.com>
2025-11-02 21:45:16 +0530

    addeed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import Home from "./pages/Home"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Basic from "./pages/basic";
 
 function App() {
-
   return (
-    <>
-     <Home />
-    </>
-  )
+    <Router>
+      <Routes>
+       
+        <Route path="/" element={<Home />} />
+
+      
+        <Route path="/basic" element={<Basic />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/basic.jsx
@@ -0,0 +1,16 @@
+import InnerBannerSection from "../components/Service/Basic/InnerBannerSection"
+import ClassOverviewSection from "../components/Service/Basic/ClassOverviewSection"
+import MembershipSection from "../components/Service/Basic/MembershipSection"
+import LimitedTimeOffer from "../components/Service/Basic/LimitedTimeOffer"
+const Basic = () => {
+    return (
+        <div>
+     <InnerBannerSection />
+     <ClassOverviewSection />
+     <MembershipSection />
+     <LimitedTimeOffer />
+        </div>
+    )
+}
+
+export default Basic
